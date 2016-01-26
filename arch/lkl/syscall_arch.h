e576be4e989f463dce1470eb1e5375c33cc94c9c
Mark Stillwell <mark@stillwell.me>
2016-01-26 10:13:20 +0000

    disable musl syscall messages for now...

--- arch/lkl/syscall_arch.h
+++ arch/lkl/syscall_arch.h
@@ -11,7 +11,7 @@ typedef long syscall_arg_t;
 /* #include <asm/syscalls.h> */
 long lkl_syscall(long no, long *params);
 
-int rumpns_printk(const char *fmt, ...);
+//int rumpns_printk(const char *fmt, ...);
 
 #define __SYSCALL_LL_E(x) (x)
 #define __SYSCALL_LL_O(x) (x)
@@ -20,7 +20,7 @@ static __inline long __syscall0(long n)
 {
 	long params[6];
 
-    rumpns_printk("musl __syscall0 %d\n", n);
+    //rumpns_printk("musl __syscall0 %d\n", n);
 	return lkl_syscall(n, params);
 }
 
@@ -29,7 +29,7 @@ static __inline long __syscall1(long n, long a1)
 	long params[6];
 
 	params[0] = a1;
-    rumpns_printk("musl _syscall1: %d\n", n);
+    //rumpns_printk("musl _syscall1: %d\n", n);
 	return lkl_syscall(n, params);
 }
 
@@ -39,7 +39,7 @@ static __inline long __syscall2(long n, long a1, long a2)
 
 	params[0] = a1;
 	params[1] = a2;
-    rumpns_printk("musl _syscall2: %d\n", n);
+    //rumpns_printk("musl _syscall2: %d\n", n);
 	return lkl_syscall(n, params);
 }
 
@@ -50,7 +50,7 @@ static __inline long __syscall3(long n, long a1, long a2, long a3)
 	params[0] = a1;
 	params[1] = a2;
 	params[2] = a3;
-    rumpns_printk("musl _syscall3: %d\n", n);
+    //rumpns_printk("musl _syscall3: %d\n", n);
 	return lkl_syscall(n, params);
 }
 
@@ -62,7 +62,7 @@ static __inline long __syscall4(long n, long a1, long a2, long a3, long a4)
 	params[1] = a2;
 	params[2] = a3;
 	params[3] = a4;
-    rumpns_printk("musl _syscall4: %d\n", n);
+    //rumpns_printk("musl _syscall4: %d\n", n);
 	return lkl_syscall(n, params);
 }
 
@@ -76,7 +76,7 @@ static __inline long __syscall5(long n, long a1, long a2, long a3, long a4,
 	params[2] = a3;
 	params[3] = a4;
 	params[4] = a5;
-    rumpns_printk("musl _syscall5: %d\n", n);
+    //rumpns_printk("musl _syscall5: %d\n", n);
 	return lkl_syscall(n, params);
 }
 
@@ -91,6 +91,6 @@ static __inline long __syscall6(long n, long a1, long a2, long a3, long a4,
 	params[3] = a4;
 	params[4] = a5;
 	params[5] = a6;
-    rumpns_printk("musl _syscall6: %d\n", n);
+    //rumpns_printk("musl _syscall6: %d\n", n);
 	return lkl_syscall(n, params);
 }
